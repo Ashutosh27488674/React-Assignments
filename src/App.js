9375f4a4bf481d3c010c807b88757734cf7337ca
Ashutosh Kumar <ashutosh26108300@gmail.com>
2022-05-21 11:33:42 +0530

    issue for multiple components resolved

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 
-function App() {
+export function App() {
   return (
-<div> <h1 data-ns-test={'project-name'}>Amazon Clone </h1> 
-<p data-ns-test={'project-description'}>This Amazon Prime Clone</p>
-</div>  
+    <div>
+
+<Name />  
+<Desc />
+    </div>
   );
 }
-
 export default App;
+
+function Desc(){
+return (
+  <div><p data-ns-test={'project-description'}>This Amazon Prime Clone</p></div>
+);
+}
+function Name(){
+  return (
+    <div> <h1 data-ns-test={'project-name'}>Amazon Clone </h1> 
+
+</div>
+  )
+}
