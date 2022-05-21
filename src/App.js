e10f333272afbbf46a89c802295342cf349c09f8
Ashutosh Kumar <ashutosh26108300@gmail.com>
2022-05-21 10:52:02 +0530

    Multiple Components

---
+++ src/App.js
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+<div> <h1 data-ns-test={'project-name'}>Amazon Clone </h1> 
+<p data-ns-test={'project-description'}>This Amazon Prime Clone</p>
+</div>  
+  );
+}
+
+export default App;
